60f46dc9539323cd0315ecc77d4e1ff8cbb90cad
ovkundik <ovkundik@gmail.com>
2024-11-21 01:48:56 +0300

    Функция загрузки изобажения

--- Картинки(Катаас).py
+++ Картинки(Катаас).py
@@ -3,9 +3,20 @@ import requests
 from PIL import Image,ImageTk
 from io import BytesIO
 
-def load_image(url)
-    response=requests.get()
-    print(response)
+from pygame.examples.moveit import load_image
+
+
+def load_img(url):
+    response=requests.get(url)
+   # print(response)
+   # print(response.headers)
+   # print(response.content) #Пришла картинка в закодированном виде
+    image_data=BytesIO(response.content)
+    print(image_data)
+    img=Image.open(image_data)
+    print(img)
+    return ImageTk.PhotoImage(img)
+
 
 window=Tk()
 window.geometry("500x500")
@@ -19,5 +30,7 @@ img=load_img(url)
 if img:
     label.config(image=img)
     #label.image=img
+
+Knopka=Button(text="Загрузка картинки", command=load_image)
 
 window.mainloop()
